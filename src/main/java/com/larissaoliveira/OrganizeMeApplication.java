805d4cb4d66ef279640db0c5ca934244e3fb78bc
Larissa Oliveira <lah.rio123@gmail.com>
2024-12-29 13:06:29 -0300

    Criando estrutura de pastas

---
+++ src/main/java/com/larissaoliveira/OrganizeMeApplication.java
@@ -0,0 +1,13 @@
+package com.larissaoliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/larissaoliveira/OrganizeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larissaoliveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
