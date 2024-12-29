f429b2d2eaf5487d4f3746917e3a943931dc4d08
Larissa Oliveira <lah.rio123@gmail.com>
2024-12-29 13:14:21 -0300

    "Criação da entidade"

---
+++ src/main/java/com/larissaoliveira/entity/Todo.java
@@ -0,0 +1,26 @@
+package com.larissaoliveira.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "todos")
+public class Todo {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    private boolean realizado;
+
+    private int prioridade;
+
+
+}
